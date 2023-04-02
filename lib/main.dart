6ff0c953b3d8882052648acff23815d6e09c29c1
clean17 <piw940317@gmail.com>
2023-04-02 21:53:53 +0900

    슬리퍼 리스트 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -27,27 +27,57 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            snap: true,
-            // floating과 함께 사용 조금만 아래로 터치하면 앱바가 전부 내려온다.
-            floating: true,
-            title: Text("SliverAppBar"),
-            pinned: false,
-            // 상단 앱바를 스크롤 내릴때만 나타나게
-            expandedHeight: 250,
-            // 내리면 어디까지 정보를 나타낼지 적용 ->flexible space
-            flexibleSpace: Container(
-              child: const Center(
-                  child: Image(image: AssetImage("assets/다운로드 (1).jpg"))),
-            ),
-            // 디폴트 60
-            centerTitle: true,
+          // SliverAppBar(
+          //   snap: true,
+          //   // floating과 함께 사용 조금만 아래로 터치하면 앱바가 전부 내려온다.
+          //   floating: true,
+          //   title: Text("SliverAppBar"),
+          //   pinned: false,
+          //   // 상단 앱바를 스크롤 내릴때만 나타나게
+          //   expandedHeight: 250,
+          //   // 내리면 어디까지 정보를 나타낼지 적용 ->flexible space
+          //   flexibleSpace: Container(
+          //     child: Center(
+          //         child:
+          //         // Image(image: AssetImage("assets/다운로드 (1).jpg"))
+          //       Image.network("https://picsum.photos/200/300", fit: BoxFit.cover)
+          //     ),
+          //   ),
+          //   // 디폴트 60
+          //   centerTitle: true,
+          // ),
+          // const SliverAppBar(
+          //   title: Text("pinned"),
+          //   pinned: true, // 상단 앱바를 스크롤 내릴때만 나타나게
+          //   centerTitle: true,
+          // ),
+          SliverList(
+              // delegate: SliverChildListDelegate(
+              //   [
+              //     Container(
+              //       child: Image(image: NetworkImage('https://picsum.photos/id/1/200/300'),fit: BoxFit.cover),
+              //     ),
+              //     SizedBox(height: 10,),
+              //     Container(
+              //       child: Image(image: NetworkImage('https://picsum.photos/id/2/200/300'),fit: BoxFit.cover),
+              //     ),
+              //     SizedBox(height: 10,),
+              //     Container(
+              //       child: Image(image: NetworkImage('https://picsum.photos/id/3/200/300'),fit: BoxFit.cover),
+              //     ),
+              //   ]
+              // ),
+            delegate: SliverChildBuilderDelegate(
+              childCount: 10,
+              (context, index) =>
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: Image.network("https://picsum.photos/id/${index}/200/300",fit: BoxFit.contain,),
+                ),
+            )
           ),
-          const SliverAppBar(
-            title: Text("pinned"),
-            pinned: true, // 상단 앱바를 스크롤 내릴때만 나타나게
-            centerTitle: true,
-          ),
+
+
           SliverToBoxAdapter(
               // 스크롤 마지막에만 보이게 된다. 일반적인 위젯을 슬리버 안에 사용할수 있게 해준다.
               // 예를 들어 컨테이너는 이러한 슬리버 안에 사용불가지만 어뎁터가 연결시켜줌
@@ -62,6 +92,8 @@ class HomePage extends StatelessWidget {
               },
             ),
           )),
+
+
           SliverFixedExtentList(
               itemExtent: 100, // 내부 요소의 크기지정을 무시한다.
               delegate: SliverChildBuilderDelegate(
@@ -72,6 +104,7 @@ class HomePage extends StatelessWidget {
                   return Diary(index);
                 },
               )),
+
         ],
       ),
     );
